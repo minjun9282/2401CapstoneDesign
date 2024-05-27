e8f2349929cc5527d73207d0d515a07dcf26f4a8
minjun9282 <54523704+minjun9282@users.noreply.github.com>
2024-05-27 09:13:21 +0900

    Add files via upload

---
+++ preprocess_vids_and_extract_frames/trim_vids_and_extract_frames3.py
@@ -0,0 +1,42 @@
+from moviepy.editor import VideoFileClip
+from PIL import Image
+import os
+import numpy as np
+from concurrent.futures import ProcessPoolExecutor
+
+# 병렬 처리를 위해 비디오 처리를 수행하는 함수를 개별적으로 정의
+def process_video(video_path, output_frame_path, base_video_frames, front_adjustment, resize_size, fps=29.97):
+    video_name = os.path.splitext(os.path.basename(video_path))[0]
+    frames_folder = os.path.join(output_frame_path, video_name + '_frames')
+    os.makedirs(frames_folder, exist_ok=True)
+    with VideoFileClip(video_path) as video:
+        # 영상의 앞부분이 기준 영상보다 짧을 경우 시작 부분에 추가될 검은색 프레임의 수 계산
+        front_black_frames = int(abs(front_adjustment) * fps) if front_adjustment < 0 else 0
+        # 영상의 앞부분이 기준 영상보다 짧을 경우 시작 부분에 추가될 검은색 프레임
+        black_frame = np.zeros((video.size[1], video.size[0], 3), dtype=np.uint8)
+        for i in range(base_video_frames): #추출될 프레임의 갯수를 base_video_frames의 갯수에 맞춰줌
+            if front_black_frames != 0 and i < front_black_frames:
+                frame = black_frame
+            else:
+                frame_time = i / fps - abs(front_adjustment) if front_adjustment < 0 else i / fps + front_adjustment
+                frame = video.get_frame(frame_time)
+
+            frame_image = Image.fromarray(frame).resize(resize_size, Image.Resampling.LANCZOS)
+            frame_path = os.path.join(frames_folder, f"frame_{i:05d}.jpg")
+            frame_image.save(frame_path, 'JPEG', quality=95)
+
+def trim_and_extract_frames_parallel(input_video_paths, output_frame_paths, front_adjustment_lst, resize_size, music_length, fps=29.97):
+    video_files = [f for f in os.listdir(input_video_paths) if f.endswith('.mp4')]
+    video_paths = [os.path.join(input_video_paths, f) for f in video_files]
+    front_adjustment_lst = [0] + front_adjustment_lst #비교 영상이 기준 영상에 비해 얼마나 지연됐는가에 대한 변수
+
+    base_video_frames = int(music_length * fps)
+
+    # 병렬 처리를 위해 ProcessPoolExecutor 사용
+    with ProcessPoolExecutor() as executor:
+        futures = [executor.submit(process_video, video_path, output_frame_paths, base_video_frames, front_adjustment_lst[idx], resize_size, fps)
+                   for idx, video_path in enumerate(video_paths)]
+
+        # 모든 작업이 완료될 때까지 기다림
+        for future in futures:
+            future.result()  # 에러가 발생하면 여기서 발생함
